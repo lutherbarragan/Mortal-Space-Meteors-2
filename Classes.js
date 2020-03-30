62e65637274bdebe70789e3aca389b33a0afff9d
Luther <luther0211@hotmail.com>
2020-03-30 18:40:10 -0500

    Moved Classes to separate file

---
+++ Classes.js
@@ -0,0 +1,16 @@
+// Classes
+class Board {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.width = canvas.width;
+		this.height = canvas.height;
+		this.img = new Image();
+		this.img.src = imgs.background;
+		this.img.onload = this.draw;
+	}
+
+	draw = () => {
+		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+	};
+}
--- main.js
+++ main.js
@@ -14,23 +14,6 @@ const imgs = {
 canvas.width = window.visualViewport.width;
 canvas.height = window.visualViewport.height;
 
-// Classes
-class Board {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-		this.width = canvas.width;
-		this.height = canvas.height;
-		this.img = new Image();
-		this.img.src = imgs.background;
-		this.img.onload = this.draw;
-	}
-
-	draw = () => {
-		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-	};
-}
-
 // instances
 let board = new Board();
 
