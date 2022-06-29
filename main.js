f6dbb9d1fa29465c1511ffd3b530dcf9f31f8d2e
Luther Barragan <luther0211@gmail.com>
2022-06-28 21:15:29 -0500

    Reduced number of meteor sizes to 3: sm, md, and Lg and changed their initial spawn speed from 1000 to 800

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext('2d');
 const timer = document.getElementById('timer');
 const score = document.getElementById('score');
 const topScore = document.getElementById('topScore');
-const xlgScore = document.getElementById('xlg');
 const lgScore = document.getElementById('lg');
 const mdScore = document.getElementById('md');
 const smScore = document.getElementById('sm');
@@ -16,7 +15,7 @@ let meteorSpawner;
 let time = 0;
 let isRunning = false;
 let frames = 0;
-let spawnSpeed = 1000;
+let spawnSpeed = 800;
 let savedData = {
 	score: 0,
 	time: 0,
@@ -47,42 +46,34 @@ const meteorScore = {
 	sm: 0,
 	md: 0,
 	lg: 0,
-	xlg: 0,
 };
 const meteorTypes = [
 	{
 		width: 100,
-		speed: 10,
+		speed: 8,
 		hp: 2,
 		value: 1000,
 		size: 'sm',
 	},
 	{
-		width: 150,
-		speed: 6,
-		hp: 3,
+		width: 250,
+		speed: 5,
+		hp: 4,
 		value: 2000,
 		size: 'md',
 	},
 	{
-		width: 200,
-		speed: 4,
-		hp: 4,
-		value: 4000,
-		size: 'lg',
-	},
-	{
-		width: 250,
+		width: 400,
 		speed: 2,
-		hp: 5,
+		hp: 6,
 		value: 5000,
-		size: 'xlg',
+		size: 'lg',
 	},
 ];
 
 // Aux Functions
 function spawnMeteor() {
-	const newMeteor = meteorTypes[Math.floor(Math.random() * 4)];
+	const newMeteor = meteorTypes[Math.floor(Math.random() * 3)];
 	meteors.push(new Meteor(newMeteor));
 }
 
@@ -158,7 +149,6 @@ function update() {
 
 	// 0.01 second (fps speed)
 	score.innerText = `Score: ${player1.score}`;
-	xlgScore.childNodes[1].innerText = meteorScore.xlg;
 	lgScore.childNodes[1].innerText = meteorScore.lg;
 	mdScore.childNodes[1].innerText = meteorScore.md;
 	smScore.childNodes[1].innerText = meteorScore.sm;
