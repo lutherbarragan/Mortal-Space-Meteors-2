91e9eb11015a5f70013a4631328338ec269ea222
Luther Barragan <luther0211@gmail.com>
2022-06-30 20:56:00 -0500

    Updated Meteor's score value, timer's score value, Score's styling

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const BODY = document.querySelector('body');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const timer = document.getElementById('timer');
-const score = document.getElementById('score');
+const scores = document.getElementById('scores');
 const topScore = document.getElementById('topScore');
 const lgScore = document.getElementById('lg');
 const mdScore = document.getElementById('md');
@@ -51,21 +51,21 @@ const meteorTypes = [
 		width: 150,
 		speed: 8,
 		hp: 2,
-		value: 1000,
+		value: 10,
 		size: 'sm',
 	},
 	{
 		width: 250,
 		speed: 5,
 		hp: 4,
-		value: 2000,
+		value: 50,
 		size: 'md',
 	},
 	{
 		width: 350,
 		speed: 2,
 		hp: 6,
-		value: 5000,
+		value: 100,
 		size: 'lg',
 	},
 ];
@@ -102,9 +102,9 @@ function start() {
 
 	isRunning = true;
 	timer.style.top = '50px';
-	score.style.display = 'block';
+	scores.style.display = 'block';
 	topScore.style.display = 'block';
-	topScore.childNodes[3].innerText = savedData.score;
+	topScore.innerText = savedData.score;
 	meteorScores.style.display = 'flex';
 	timer.innerText = time;
 	pauseScreen.style.display = 'none';
@@ -115,14 +115,16 @@ function start() {
 
 function update() {
 	frames++;
-	player1.score += 1;
+
+	// 10 points per second
+	if (frames % 10 == 0) player1.score += 1;
 
 	if (savedData.score < player1.score) {
 		savedData.score = player1.score;
 		savedData.time = time;
 		savedData.meteors = { ...meteorScore };
 
-		topScore.childNodes[3].innerText = savedData.score;
+		topScore.innerText = savedData.score;
 
 		localStorage.setItem('MSM2TopScore', JSON.stringify(savedData));
 	}
@@ -147,7 +149,7 @@ function update() {
 	}
 
 	// 0.01 second (fps speed)
-	score.innerText = `Score: ${player1.score}`;
+	score.innerText = `${player1.score}`;
 	lgScore.childNodes[1].innerText = meteorScore.lg;
 	mdScore.childNodes[1].innerText = meteorScore.md;
 	smScore.childNodes[1].innerText = meteorScore.sm;
